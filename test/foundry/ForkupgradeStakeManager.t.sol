4b5c80da07d4deeae990a4ef3d66c674ab556460
ethyla <36268899+ethyla@users.noreply.github.com>
2024-07-22 21:19:36 +0200

    test: extend upgrade forktest

--- test/foundry/ForkupgradeStakeManager.t.sol
+++ test/foundry/ForkupgradeStakeManager.t.sol
@@ -1,11 +1,12 @@
 pragma solidity ^0.8.4;
 
-import { StakeManager } from "../../scripts/helpers/interfaces/StakeManager.generated.sol";
-import { StakeManagerProxy } from "../../scripts/helpers/interfaces/StakeManagerProxy.generated.sol";
-import { ValidatorShare } from "../../scripts/helpers/interfaces/ValidatorShare.generated.sol";
+import {StakeManager} from "../../scripts/helpers/interfaces/StakeManager.generated.sol";
+import {StakeManagerProxy} from "../../scripts/helpers/interfaces/StakeManagerProxy.generated.sol";
+import {ValidatorShare} from "../../scripts/helpers/interfaces/ValidatorShare.generated.sol";
 import {DepositManager} from "../../scripts/helpers/interfaces/DepositManager.generated.sol";
-import { Registry } from "../../scripts/helpers/interfaces/Registry.generated.sol";
-import { ERC20 } from "../../scripts/helpers/interfaces/ERC20.generated.sol";
+import {Registry} from "../../scripts/helpers/interfaces/Registry.generated.sol";
+import {ERC20} from "../../scripts/helpers/interfaces/ERC20.generated.sol";
+import {Proxy} from "../../scripts/helpers/interfaces/Proxy.generated.sol";
 
 import {UpgradeStake_DepositManager_Mainnet} from "../../scripts/deployers/pol-upgrade/UpgradeStake_DepositManager_Mainnet.s.sol";
 import {Timelock} from "../../contracts/common/misc/ITimelock.sol";
@@ -27,7 +28,8 @@ contract ForkupgradeStakeManagerTest is Test, UpgradeStake_DepositManager_Mainne
 
         loadConfig();
         (StakeManager stakeManagerImpl, ValidatorShare validatorShareImpl, DepositManager depositManagerImpl) = deployImplementations(deployerPrivateKey);
-        (bytes memory scheduleBatchPayload, bytes memory executeBatchPayload, bytes32 payloadId) = createPayload(stakeManagerImpl, validatorShareImpl, depositManagerImpl);       
+        (bytes memory scheduleBatchPayload, bytes memory executeBatchPayload, bytes32 payloadId) =
+            createPayload(stakeManagerImpl, validatorShareImpl, depositManagerImpl);
 
         uint256 balanceStakeManager = maticToken.balanceOf(address(stakeManagerProxy));
         console.log("Initial StakeManager Matic balance: ", balanceStakeManager);
@@ -40,7 +42,7 @@ contract ForkupgradeStakeManagerTest is Test, UpgradeStake_DepositManager_Mainne
         (bool successSchedule, bytes memory dataSchedule) = address(timelock).call(scheduleBatchPayload);
         if (successSchedule == false) {
             assembly {
-            revert(add(dataSchedule,32),mload(dataSchedule))
+                revert(add(dataSchedule, 32), mload(dataSchedule))
             }
         }
 
@@ -48,7 +50,7 @@ contract ForkupgradeStakeManagerTest is Test, UpgradeStake_DepositManager_Mainne
         assertEq(timelock.isOperation(payloadId), true);
         assertEq(timelock.isOperationPending(payloadId), true);
 
-        vm.warp(block.timestamp + 172800);
+        vm.warp(block.timestamp + 172_800);
 
         assertEq(timelock.isOperationReady(payloadId), true);
 
@@ -57,15 +59,29 @@ contract ForkupgradeStakeManagerTest is Test, UpgradeStake_DepositManager_Mainne
         (bool successExecute, bytes memory dataExecute) = address(timelock).call(executeBatchPayload);
         if (successExecute == false) {
             assembly {
-            revert(add(dataExecute,32),mload(dataExecute))
+                revert(add(dataExecute, 32), mload(dataExecute))
             }
         }
         assertEq(successExecute, true);
         assertEq(timelock.isOperationDone(payloadId), true);
 
+        // Check migrations happened
         assertEq(maticToken.balanceOf(address(stakeManagerProxy)), 0);
         assertEq(polToken.balanceOf(address(stakeManagerProxy)), balanceStakeManager);
-        assertEq(maticToken.balanceOf(address(depositManagerProxy)), balanceDepositManager/2);
-        assertEq(polToken.balanceOf(address(stakeManagerProxy)), balanceDepositManager/2);
+        assertEq(maticToken.balanceOf(address(depositManagerProxy)), 0);
+        assertEq(polToken.balanceOf(address(depositManagerProxy)), balanceDepositManager);
+
+        // Check Registry values
+        assertEq(registry.contractMap(keccak256("validatorShare")), address(validatorShareImpl));
+        assertEq(registry.contractMap(keccak256("pol")), address(polToken));
+        assertEq(registry.contractMap(keccak256("matic")), address(maticToken));
+        assertEq(registry.contractMap(keccak256("polygonMigration")), migrationAddress);
+        assertEq(registry.rootToChildToken(address(polToken)), nativeGasTokenAddress);
+        assertEq(registry.childToRootToken(nativeGasTokenAddress), address(polToken));
+        assertEq(registry.isERC721(address(polToken)), false);
+
+        // Check Proxy implementation addresses
+        assertEq(Proxy(payable(address(stakeManagerProxy))).implementation(), address(stakeManagerImpl));
+        assertEq(Proxy(payable(address(depositManagerProxy))).implementation(), address(depositManagerImpl));
     }
 }
