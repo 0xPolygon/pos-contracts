4336d7bbf05e5cad592cc5bf23ace97eff14eb9e
ethyla <36268899+ethyla@users.noreply.github.com>
2024-07-22 13:51:04 +0200

    feat: improve mainnet deploy script

--- scripts/deployers/pol-upgrade/UpgradeStake_DepositManager_Mainnet.s.sol
+++ scripts/deployers/pol-upgrade/UpgradeStake_DepositManager_Mainnet.s.sol
@@ -17,92 +17,117 @@ import {Timelock} from "../../../contracts/common/misc/ITimelock.sol";
 contract UpgradeStake_DepositManager_Mainnet is Script {
     using stdJson for string;
 
+    Timelock timelock;
+    Registry registry;
+    StakeManager stakeManagerProxy;
+    Governance governance;
+    ERC20 polToken;
+    address migrationAddress;
+    DepositManager depositManagerProxy;
+    ERC20 maticToken;
+    address nativeGasTokenAddress;
+    address gSafeAddress;
+
     function run() public {
         uint256 deployerPrivateKey = vm.promptSecretUint("Enter deployer private key: ");
+        //uint256 deployerPrivateKey = vm.envUint("PRIVATE_KEY");
+        loadConfig();
+        (StakeManager stakeManagerImpl, ValidatorShare validatorShareImpl, DepositManager depositManagerImpl) = deployImplementations(deployerPrivateKey);
+        (bytes memory scheduleBatchPayload, bytes memory executeBatchPayload, bytes32 payloadId) =
+            createPayload(stakeManagerImpl, validatorShareImpl, depositManagerImpl);
+
+        console.log("Send scheduleBatchPayload to: ", address(timelock));
+        console.logBytes(scheduleBatchPayload);
+
+        console.log("----------------------");
+        console.log("After at least 7 days send executeBatchPayload to: ", address(timelock));
+        console.logBytes(executeBatchPayload);
+    }
+
+    function loadConfig() public {
+        console.log("----------------------");
+        console.log("Loading config \n");
 
         string memory input = vm.readFile("scripts/deployers/pol-upgrade/input.json");
         string memory chainIdSlug = string(abi.encodePacked('["', vm.toString(block.chainid), '"]'));
-        address registryAddress = input.readAddress(string.concat(chainIdSlug, ".registry"));
-        address stakeManagerProxyAddress = input.readAddress(string.concat(chainIdSlug, ".stakeManagerProxy"));
-        address governanceAddress = input.readAddress(string.concat(chainIdSlug, ".governance"));
-        address polTokenAddress = input.readAddress(string.concat(chainIdSlug, ".polToken"));
-        address migrationAddress = input.readAddress(string.concat(chainIdSlug, ".migration"));
-        address timelockAddress = input.readAddress(string.concat(chainIdSlug, ".timelock"));
-        address payable depositManagerProxyAddress = payable(input.readAddress(string.concat(chainIdSlug, ".depositManagerProxy")));
-        address maticAddress = input.readAddress(string.concat(chainIdSlug, ".matic"));
-        address nativeGasTokenAddress = address(0x0000000000000000000000000000000000001010);
 
+        registry = Registry(input.readAddress(string.concat(chainIdSlug, ".registry")));
+        stakeManagerProxy = StakeManager(input.readAddress(string.concat(chainIdSlug, ".stakeManagerProxy")));
+        governance = Governance(input.readAddress(string.concat(chainIdSlug, ".governance")));
+        polToken = ERC20(input.readAddress(string.concat(chainIdSlug, ".polToken")));
+        migrationAddress = input.readAddress(string.concat(chainIdSlug, ".migration"));
+        timelock = Timelock(payable(input.readAddress(string.concat(chainIdSlug, ".timelock"))));
+        depositManagerProxy = DepositManager(payable(input.readAddress(string.concat(chainIdSlug, ".depositManagerProxy"))));
+        maticToken = ERC20(input.readAddress(string.concat(chainIdSlug, ".matic")));
+        nativeGasTokenAddress = input.readAddress(string.concat(chainIdSlug, ".nativGasToken"));
+        gSafeAddress = input.readAddress(string.concat(chainIdSlug, ".gSafe"));
+
+        console.log("using Registry at: ", address(registry));
+        console.log("using StakeManagerProxy at: ", address(stakeManagerProxy));
+        console.log("using DepositManagerProxy at: ", address(depositManagerProxy));
+        console.log("using Governance at: ", address(governance));
+        console.log("using Timelock at: ", address(timelock));
+        console.log("using Matic at: ", address(maticToken));
+        console.log("using POL at: ", address(polToken));
+        console.log("using PolygonMigration at: ", migrationAddress);
+        console.log("using NativGasToken at: ", nativeGasTokenAddress);
+        console.log("using gSafe at: ", gSafeAddress);
+    }
+
+    function deployImplementations(uint256 deployerPrivateKey)
+        public
+        returns (StakeManager stakeManagerImpl, ValidatorShare validatorShareImpl, DepositManager depositManagerImpl)
+    {
         vm.startBroadcast(deployerPrivateKey);
 
         // deploy STEP 1
         // deploy new StakeManager version
-        StakeManager stakeManagerImpl;
         stakeManagerImpl = StakeManager(deployCode("out/StakeManager.sol/StakeManager.json"));
 
         console.log("deployed StakeManager implementation at: ", address(stakeManagerImpl));
 
         // deploy STEP 2
         // deploy new ValidatorShare version
-        ValidatorShare validatorShareImpl;
         validatorShareImpl = ValidatorShare(deployCode("out/ValidatorShare.sol/ValidatorShare.json"));
 
         console.log("deployed ValidatorShare implementation at: ", address(validatorShareImpl));
 
         // deploy STEP 3
-        // deploy new DepositManager version 
-        DepositManager depositManagerImpl;
+        // deploy new DepositManager version
         depositManagerImpl = DepositManager(payable(deployCode("out/DepositManager.sol/DepositManager.json")));
 
         console.log("deployed DepositManager implementation at: ", address(validatorShareImpl));
 
         vm.stopBroadcast();
+    }
 
-        Registry registry = Registry(registryAddress);
-        console.log("using Registry at: ", address(registry));
-
-        StakeManager stakeManager = StakeManager(stakeManagerProxyAddress);
-        StakeManagerProxy stakeManagerProxy = StakeManagerProxy(payable(stakeManagerProxyAddress));
-        console.log("using StakeManagerProxy at: ", address(stakeManagerProxy));
-
-        DepositManager depositManager = DepositManager(depositManagerProxyAddress);
-        DepositManagerProxy depositManagerProxy = DepositManagerProxy(depositManagerProxyAddress);
-        console.log("using DepositManagerProxy at: ", address(depositManagerProxy));
-
-        Governance governance = Governance(governanceAddress);
-        console.log("using Governance at: ", address(governanceAddress));
-
-        Timelock timelock = Timelock(payable(timelockAddress));
-        console.log("using Timelock at: ", address(timelockAddress));
-
-        ERC20 maticToken = ERC20(maticAddress);
-        console.log("using Matic at: ", address(maticAddress));
-
+    function createPayload(StakeManager stakeManagerImpl, ValidatorShare validatorShareImpl, DepositManager depositManagerImpl)
+        public
+        returns (bytes memory scheduleBatchPayload, bytes memory executeBatchPayload, bytes32 payloadId)
+    {
         console.log("----------------------");
         console.log("Generating payloads \n");
 
-        // STEP 1 
+        // STEP 1
         // Update ValidatorShare registry entry
-        bytes memory payloadRegistry1 = abi.encodeWithSelector(
-            governance.update.selector,
-            address(registry),
-            abi.encodeWithSelector(registry.updateContractMap.selector, keccak256("validatorShare"), address(validatorShareImpl))
+        bytes memory payloadRegistry1 = abi.encodeCall(
+            governance.update, (address(registry), abi.encodeCall(registry.updateContractMap, (keccak256("validatorShare"), address(validatorShareImpl))))
         );
 
         console.log("Created payloadRegistry1 for: ", address(governance));
         console.logBytes(payloadRegistry1);
 
-
         // STEP 2
         // Update StakeManagerProxy implementation contract
-        bytes memory payloadStakeManager2 = abi.encodeWithSelector(stakeManagerProxy.updateImplementation.selector, address(stakeManagerImpl));
+        bytes memory payloadStakeManager2 = abi.encodeCall(StakeManagerProxy.updateImplementation, (address(stakeManagerImpl)));
 
         console.log("Created payloadStakeManager2 for: ", address(stakeManagerProxy));
         console.logBytes(payloadStakeManager2);
 
         // STEP 3
         // Call initializePOL
-        bytes memory payloadInitializePol3 = abi.encodeWithSelector(
-            governance.update.selector, address(stakeManagerProxy), abi.encodeWithSelector(stakeManager.initializePOL.selector, polTokenAddress, migrationAddress)
+        bytes memory payloadInitializePol3 = abi.encodeCall(
+            governance.update, (address(stakeManagerProxy), abi.encodeCall(stakeManagerProxy.initializePOL, (address(polToken), migrationAddress)))
         );
 
         console.log("Created payloadInitializePol3 for: ", address(governance));
@@ -110,26 +135,24 @@ contract UpgradeStake_DepositManager_Mainnet is Script {
 
         // STEP 4
         // Call updateContractMap on registry to add "pol"
-        bytes memory payloadContractMapPol4 = abi.encodeWithSelector(
-            governance.update.selector, address(registry), abi.encodeWithSelector(registry.updateContractMap.selector, keccak256("pol"), polTokenAddress)
-        );
+        bytes memory payloadContractMapPol4 =
+            abi.encodeCall(governance.update, (address(registry), abi.encodeCall(registry.updateContractMap, (keccak256("pol"), address(polToken)))));
 
         console.log("Send payloadContractMapPol4 to: ", address(governance));
         console.logBytes(payloadContractMapPol4);
 
         // STEP 5
         // Call updateContractMap on registry to add "matic"
-        bytes memory payloadContractMapMatic5 = abi.encodeWithSelector(
-            governance.update.selector, address(registry), abi.encodeWithSelector(registry.updateContractMap.selector, keccak256("matic"), maticAddress)
-        );
+        bytes memory payloadContractMapMatic5 =
+            abi.encodeCall(governance.update, (address(registry), abi.encodeCall(registry.updateContractMap, (keccak256("matic"), address(maticToken)))));
 
-        console.log("\n Send payloadContractMapMatic5 to: ", address(governance));
+        console.log("Send payloadContractMapMatic5 to: ", address(governance));
         console.logBytes(payloadContractMapMatic5);
 
         // STEP 6
         // Call updateContractMap on registry to add "polygonMigration"
-        bytes memory payloadContractMapMigration6 = abi.encodeWithSelector(
-            governance.update.selector, address(registry), abi.encodeWithSelector(registry.updateContractMap.selector, keccak256("polygonMigration"), migrationAddress)
+        bytes memory payloadContractMapMigration6 = abi.encodeCall(
+            governance.update, (address(registry), abi.encodeCall(registry.updateContractMap, (keccak256("polygonMigration"), migrationAddress)))
         );
 
         console.log("Send payloadContractMapMigration6 to: ", address(governance));
@@ -137,8 +160,8 @@ contract UpgradeStake_DepositManager_Mainnet is Script {
 
         // STEP 7
         // call mapToken on the Registry to map POL to the PoS native gas token address (1010)
-        bytes memory payloadMapToken7 = abi.encodeWithSelector(
-            governance.update.selector, address(registry), abi.encodeWithSelector(registry.mapToken.selector, polTokenAddress, nativeGasTokenAddress, false)
+        bytes memory payloadMapToken7 = abi.encodeCall(
+            governance.update.selector, (address(registry), abi.encodeCall(registry.mapToken, (address(polToken), nativeGasTokenAddress, false)))
         );
 
         console.log("Send payloadMapToken7 to: ", address(governance));
@@ -146,7 +169,7 @@ contract UpgradeStake_DepositManager_Mainnet is Script {
 
         // STEP 8
         // update impl of proxy to DepositManager
-        bytes memory payloadUpgradeDepositManager8 = abi.encodeWithSelector(depositManagerProxy.updateImplementation.selector, address(depositManagerImpl));
+        bytes memory payloadUpgradeDepositManager8 = abi.encodeCall(DepositManagerProxy.updateImplementation, (address(depositManagerImpl)));
 
         console.log();
         console.log("Send payloadUpgradeDepositManager8 to: ", address(depositManagerProxy));
@@ -155,11 +178,10 @@ contract UpgradeStake_DepositManager_Mainnet is Script {
         // STEP 9
         // call migrateMatic on the new DepositManager, migrating all MATIC
         uint256 amount = maticToken.balanceOf(address(depositManagerProxy));
-        bytes memory payloadMigrateMatic9 = abi.encodeWithSelector(
-            governance.update.selector, address(depositManagerProxy), abi.encodeWithSelector(depositManager.migrateMatic.selector, amount)
-        );
+        bytes memory payloadMigrateMatic9 =
+            abi.encodeCall(governance.update, (address(depositManagerProxy), abi.encodeCall(depositManagerProxy.migrateMatic, (amount / 2))));
 
-        console.log("\n Send payloadMigrateMatic9 to: ", address(governance));
+        console.log("Send payloadMigrateMatic9 to: ", address(governance));
         console.logBytes(payloadMigrateMatic9);
 
         console.log("----------------------");
@@ -190,18 +212,11 @@ contract UpgradeStake_DepositManager_Mainnet is Script {
         payloads[7] = payloadUpgradeDepositManager8;
         payloads[8] = payloadMigrateMatic9;
 
-        bytes memory batchPayload = abi.encodeWithSelector(Timelock.scheduleBatch.selector, targets, values, payloads, "", "");
-        bytes32 payloadId = timelock.hashOperationBatch(targets, values, payloads, "", "");
-
+        payloadId = timelock.hashOperationBatch(targets, values, payloads, "", "");
         console.log("Expected batch ID: %s", vm.toString(payloadId));
-        console.log("Send batchPayload to: ", address(timelock));
-        console.logBytes(batchPayload);
 
-        bytes memory executePayload = abi.encodeWithSelector(Timelock.executeBatch.selector, targets, values, payloads, "", "");
-
-        console.log("----------------------");
-        console.log("After at least 7 days send executePayload to: ", address(timelock));
-        console.logBytes(executePayload);
-
+        // 172800 is minDelay
+        scheduleBatchPayload = abi.encodeCall(Timelock.scheduleBatch, (targets, values, payloads, "", "", 172_800));
+        executeBatchPayload = abi.encodeCall(Timelock.executeBatch, (targets, values, payloads, "", ""));
     }
 }
