6b3c2f1e377099c52943dffca1530c5d21fec0af
ethyla <36268899+ethyla@users.noreply.github.com>
2024-07-22 13:51:38 +0200

    feat: use deploy script in upgrade forktest

--- test/foundry/ForkupgradeStakeManager.t.sol
+++ test/foundry/ForkupgradeStakeManager.t.sol
@@ -3,61 +3,69 @@ pragma solidity ^0.8.4;
 import { StakeManager } from "../../scripts/helpers/interfaces/StakeManager.generated.sol";
 import { StakeManagerProxy } from "../../scripts/helpers/interfaces/StakeManagerProxy.generated.sol";
 import { ValidatorShare } from "../../scripts/helpers/interfaces/ValidatorShare.generated.sol";
+import {DepositManager} from "../../scripts/helpers/interfaces/DepositManager.generated.sol";
 import { Registry } from "../../scripts/helpers/interfaces/Registry.generated.sol";
 import { ERC20 } from "../../scripts/helpers/interfaces/ERC20.generated.sol";
 
+import {UpgradeStake_DepositManager_Mainnet} from "../../scripts/deployers/pol-upgrade/UpgradeStake_DepositManager_Mainnet.s.sol";
+import {Timelock} from "../../contracts/common/misc/ITimelock.sol";
+
 import "forge-std/Test.sol";
 
-contract ForkupgradeStakeManagerTest is Test {
+contract ForkupgradeStakeManagerTest is Test, UpgradeStake_DepositManager_Mainnet {
     uint256 mainnetFork;
 
     function setUp() public {
         mainnetFork = vm.createFork(vm.rpcUrl("mainnet"));
+        vm.selectFork(mainnetFork);
     }
 
     function test_UpgradeStakeManager() public {
-        vm.selectFork(mainnetFork);
         assertEq(vm.activeFork(), mainnetFork);
 
-        StakeManager stakeManagerImpl;
-        stakeManagerImpl = StakeManager(deployCode("out/StakeManager.sol/StakeManager.json"));
+        uint256 deployerPrivateKey = vm.envUint("PRIVATE_KEY");
 
-        console.log("deployed StakeManager Implementation at: ", address(stakeManagerImpl));
+        loadConfig();
+        (StakeManager stakeManagerImpl, ValidatorShare validatorShareImpl, DepositManager depositManagerImpl) = deployImplementations(deployerPrivateKey);
+        (bytes memory scheduleBatchPayload, bytes memory executeBatchPayload, bytes32 payloadId) = createPayload(stakeManagerImpl, validatorShareImpl, depositManagerImpl);       
 
-        ValidatorShare validatorShareImpl;
-        validatorShareImpl = ValidatorShare(deployCode("out/ValidatorShare.sol/ValidatorShare.json"));
+        uint256 balanceStakeManager = maticToken.balanceOf(address(stakeManagerProxy));
+        console.log("Initial StakeManager Matic balance: ", balanceStakeManager);
+        assertNotEq(balanceStakeManager, 0);
+        uint256 balanceDepositManager = maticToken.balanceOf(address(depositManagerProxy));
+        console.log("Initial DepositManager Matic balance: ", balanceDepositManager);
+        assertNotEq(balanceDepositManager, 0);
 
-        console.log("deployed ValidatorShare Implementation at: ", address(validatorShareImpl));
+        vm.prank(gSafeAddress);
+        (bool successSchedule, bytes memory dataSchedule) = address(timelock).call(scheduleBatchPayload);
+        if (successSchedule == false) {
+            assembly {
+            revert(add(dataSchedule,32),mload(dataSchedule))
+            }
+        }
 
-        Registry registry = Registry(0x33a02E6cC863D393d6Bf231B697b82F6e499cA71);
-        console.log("found Registry at: ", address(registry));
+        assertEq(successSchedule, true);
+        assertEq(timelock.isOperation(payloadId), true);
+        assertEq(timelock.isOperationPending(payloadId), true);
 
-        StakeManager stakeManager = StakeManager(0x5e3Ef299fDDf15eAa0432E6e66473ace8c13D908);
-        StakeManagerProxy stakeManagerProxy = StakeManagerProxy(payable(0x5e3Ef299fDDf15eAa0432E6e66473ace8c13D908));
-        console.log("found StakeManagerProxy at: ", address(stakeManagerProxy));
+        vm.warp(block.timestamp + 172800);
 
-        address governance = 0x6e7a5820baD6cebA8Ef5ea69c0C92EbbDAc9CE48;
-        address timelock = 0xCaf0aa768A3AE1297DF20072419Db8Bb8b5C8cEf;
-        ERC20 polToken = ERC20(0x455e53CBB86018Ac2B8092FdCd39d8444aFFC3F6);
-        ERC20 maticToken = ERC20(0x7D1AfA7B718fb893dB30A3aBc0Cfc608AaCfeBB0);
-        address migration = 0x29e7DF7b6A1B2b07b731457f499E1696c60E2C4e;
+        assertEq(timelock.isOperationReady(payloadId), true);
 
-        uint256 balance = maticToken.balanceOf(address(stakeManagerProxy));
-        console.log("Matic balance: ", balance);
+        vm.prank(gSafeAddress);
 
-        // prank set registry as 0x6e7a5820baD6cebA8Ef5ea69c0C92EbbDAc9CE48
-        vm.prank(governance);
-        registry.updateContractMap(keccak256("validatorShare"), address(validatorShareImpl));
-
-        // prank set proxyImpl as 0xCaf0aa768A3AE1297DF20072419Db8Bb8b5C8cEf
-        vm.prank(timelock);
-        stakeManagerProxy.updateImplementation(address(stakeManagerImpl));
-        // prank call initPol with pol 0x455e53CBB86018Ac2B8092FdCd39d8444aFFC3F6 mig 0x29e7DF7b6A1B2b07b731457f499E1696c60E2C4e as 0x6e7a5820baD6cebA8Ef5ea69c0C92EbbDAc9CE48
-
-        vm.prank(governance);
-        stakeManager.initializePOL(address(polToken), migration);
+        (bool successExecute, bytes memory dataExecute) = address(timelock).call(executeBatchPayload);
+        if (successExecute == false) {
+            assembly {
+            revert(add(dataExecute,32),mload(dataExecute))
+            }
+        }
+        assertEq(successExecute, true);
+        assertEq(timelock.isOperationDone(payloadId), true);
 
         assertEq(maticToken.balanceOf(address(stakeManagerProxy)), 0);
-        assertEq(polToken.balanceOf(address(stakeManagerProxy)), balance);
+        assertEq(polToken.balanceOf(address(stakeManagerProxy)), balanceStakeManager);
+        assertEq(maticToken.balanceOf(address(depositManagerProxy)), balanceDepositManager/2);
+        assertEq(polToken.balanceOf(address(stakeManagerProxy)), balanceDepositManager/2);
     }
 }
