61b98b293ba895a804a77a915cabf6e14dfb9f2c
kamuikatsurgi <shahkrishang11@gmail.com>
2025-02-18 11:43:41 +0530

    forge fmt and remove broadcast files

--- scripts/matic-cli-scripts/Deposit.s.sol
+++ scripts/matic-cli-scripts/Deposit.s.sol
@@ -1,56 +1,51 @@
 // SPDX-License-Identifier: MIT
 
 pragma solidity ^0.8.4;
+
 import {Script, stdJson, console} from "forge-std/Script.sol";
 import {ChildERC20Proxified} from "../helpers/interfaces/ChildERC20Proxified.generated.sol";
 import {ChildERC721Proxified} from "../helpers/interfaces/ChildERC721Proxified.generated.sol";
 import {DepositManager} from "../helpers/interfaces/DepositManager.generated.sol";
 
 contract MaticDeposit is Script {
+    string path = "contractAddresses.json";
+    string json = vm.readFile(path);
 
-  string path = "contractAddresses.json";
-  string json = vm.readFile(path);
+    function run(address addr, address rootToken, uint256 amount) public {
+        uint256 deployerPrivateKey = vm.envUint("DEPLOYER_PRIVATE_KEY");
 
-  function run(address addr, address rootToken, uint256 amount) public {
+        vm.startBroadcast(deployerPrivateKey);
 
-    uint256 deployerPrivateKey = vm.envUint("DEPLOYER_PRIVATE_KEY"); 
+        depositERC20(addr, rootToken, amount);
 
-    vm.startBroadcast(deployerPrivateKey);
+        vm.stopBroadcast();
+    }
 
-    depositERC20(addr, rootToken, amount);
-    
+    function checkDepositedERC20Balance(address addr, address token) public {
+        ChildERC20Proxified childToken = ChildERC20Proxified(token);
+        uint256 balance = childToken.balanceOf(addr);
+        console.log("Balance of given address: ", balance);
+    }
 
-    vm.stopBroadcast();
-  }
+    function checkDepositedERC721Balance(address addr, address token, uint256 tokenID) public {
+        ChildERC721Proxified childToken = ChildERC721Proxified(token);
+        address owner = childToken.ownerOf(tokenID);
+        console.log("Owner of given NFT: ", owner, ", but should be: ", addr);
+    }
 
-  function checkDepositedERC20Balance(address addr, address token) public {
-    ChildERC20Proxified childToken = ChildERC20Proxified(token);
-    uint256 balance = childToken.balanceOf(addr);
-    console.log("Balance of given address : ", balance);
-  }
+    function depositERC20(address addr, address rootToken, uint256 amount) public {
+        console.log("Deposit ERC20: ");
+        console.log("Token: ", rootToken);
+        console.log("Amount: ", amount);
+        ChildERC20Proxified childToken = ChildERC20Proxified(rootToken);
+        uint256 b = childToken.balanceOf(addr);
+        console.log("Balance of cuurent address: ", b);
 
-  function checkDepositedERC721Balance(address addr, address token, uint256 tokenID) public {
-    ChildERC721Proxified childToken = ChildERC721Proxified(token);
-    address owner = childToken.ownerOf(tokenID);
-    console.log("Owner of given NFT : ", owner, "should be ", addr);
-  }
+        DepositManager depositManager = DepositManager(payable(vm.parseJsonAddress(json, ".root.DepositManagerProxy")));
+        childToken.approve(address(depositManager), amount);
+        console.log("Tokens Approved!");
 
-  function depositERC20(address addr, address rootToken, uint256 amount) public {
-    console.log("Deposit ERC20 : ");
-    console.log(" Token : ", rootToken);
-    console.log(" Amount : ", amount);
-    ChildERC20Proxified childToken = ChildERC20Proxified(rootToken);
-    uint256 b = childToken.balanceOf(addr);
-    console.log("Balance of cuurent address ; ", b);
-
-    DepositManager depositManager = DepositManager(payable(vm.parseJsonAddress(json, ".root.DepositManagerProxy")));
-    childToken.approve(address(depositManager), amount);
-    console.log("Tokens Approved!");
-
-    depositManager.depositERC20(rootToken, amount);
-    console.log("Success!");
-
-    
-  }
-
+        depositManager.depositERC20(rootToken, amount);
+        console.log("Success!");
+    }
 }
--- scripts/matic-cli-scripts/stake.s.sol
+++ scripts/matic-cli-scripts/stake.s.sol
@@ -1,6 +1,7 @@
 // SPDX-License-Identifier: MIT
 
 pragma solidity ^0.8.4;
+
 import {Script, stdJson, console} from "forge-std/Script.sol";
 
 import {TestToken} from "../helpers/interfaces/TestToken.generated.sol";
@@ -8,68 +9,54 @@ import {StakeManager} from "../helpers/interfaces/StakeManager.generated.sol";
 import {IERC20} from "../helpers/interfaces/IERC20.generated.sol";
 
 contract MaticStake is Script {
+    string path = "contractAddresses.json";
+    string json = vm.readFile(path);
 
-  string path = "contractAddresses.json";
-  string json = vm.readFile(path);
+    function run(address validatorAccount, bytes memory pubkey, uint256 stakeAmount, uint256 heimdallFee) public {
+        uint256 deployerPrivateKey = vm.envUint("DEPLOYER_PRIVATE_KEY");
 
-  function run(address validatorAccount, bytes memory pubkey, uint256 stakeAmount, uint256 heimdallFee) public {
+        vm.startBroadcast(deployerPrivateKey);
 
-    uint256 deployerPrivateKey = vm.envUint("DEPLOYER_PRIVATE_KEY"); 
+        stake(validatorAccount, pubkey, stakeAmount, heimdallFee);
 
-    vm.startBroadcast(deployerPrivateKey);
-    
-    stake(validatorAccount, pubkey, stakeAmount, heimdallFee);
-    // topUpForFee();
+        vm.stopBroadcast();
+    }
 
-    vm.stopBroadcast();
-  }
+    function stake(address _validatorAccount, bytes memory _pubkey, uint256 _stakeAmount, uint256 _heimdallFee) public {
+        address validatorAccount = _validatorAccount;
+        bytes memory pubkey = _pubkey;
+        uint256 stakeAmount = _stakeAmount;
+        uint256 heimdallFee = _heimdallFee;
 
-  function stake(address _validatorAccount, bytes memory _pubkey, uint256 _stakeAmount, uint256 _heimdallFee) public {
-    
-    // address validatorAccount = vm.envAddress("VALIDATOR");
-    // bytes memory pubkey = vm.envBytes("VALIDATOR_PUB_KEY");
-    // uint256 stakeAmount = vm.envUint("STAKE_AMOUNT");
-    // uint256 heimdallFee = vm.envUint("HEIMDALL_FEE");
-    address validatorAccount = _validatorAccount;
-    bytes memory pubkey = _pubkey;
-    uint256 stakeAmount = _stakeAmount;
-    uint256 heimdallFee = _heimdallFee;
+        console.log("StakeAmount: ", stakeAmount, " for validatorAccount: ", validatorAccount);
 
-    console.log("StakeAmount : ", stakeAmount, " for validatorAccount : ", validatorAccount);
+        StakeManager stakeManager = StakeManager(vm.parseJsonAddress(json, ".root.StakeManagerProxy"));
+        console.log("StakeManager address: ", address(stakeManager));
+        TestToken maticToken = TestToken(vm.parseJsonAddress(json, ".root.tokens.MaticToken"));
+        console.log("Sender account has a balance of: ", maticToken.balanceOf(validatorAccount));
 
-    StakeManager stakeManager = StakeManager(vm.parseJsonAddress(json, ".root.StakeManagerProxy"));
-    console.log("StakeManager address : ", address(stakeManager));
-    TestToken maticToken = TestToken(vm.parseJsonAddress(json, ".root.tokens.MaticToken"));
-    console.log("Sender account has a balance of : ", maticToken.balanceOf(validatorAccount));
+        maticToken.approve(address(stakeManager), 10 ** 20);
+        console.log("sent approve tx, staking now...");
 
-    maticToken.approve(address(stakeManager), 10**20);
-    console.log('sent approve tx, staking now...');
-    
-    console.log("Validator set size : ", stakeManager.currentValidatorSetSize());
+        stakeManager.stakeForPOL(validatorAccount, stakeAmount, heimdallFee, true, pubkey);
 
+        console.log("Validator set size: ", stakeManager.currentValidatorSetSize());
+    }
 
-    stakeManager.stakeForPOL(validatorAccount, stakeAmount, heimdallFee, true, pubkey);
+    function topUpForFee() public {
+        address stakeFor = vm.envAddress("VALIDATOR_1");
+        uint256 amount = 10 ** 20;
 
+        StakeManager stakeManager = StakeManager(vm.parseJsonAddress(json, ".stakeManager"));
+        TestToken rootToken = TestToken(vm.parseJsonAddress(json, ".maticToken"));
+        rootToken.approve(vm.parseJsonAddress(json, ".stakeManagerProxy"), amount);
 
+        console.log("Approved!, staking now...");
 
-  }
+        uint256 validatorId = stakeManager.signerToValidator(stakeFor);
+        console.log("Validator ID : ", validatorId);
+        stakeManager.topUpForFee(stakeFor, amount);
 
-  function topUpForFee() public {
-    address stakeFor = vm.envAddress("VALIDATOR_1");
-    uint256 amount = 10**20;
-
-    StakeManager stakeManager = StakeManager(vm.parseJsonAddress(json, ".stakeManager"));
-    TestToken rootToken = TestToken(vm.parseJsonAddress(json, ".maticToken"));
-    rootToken.approve(vm.parseJsonAddress(json, ".stakeManagerProxy"), amount);
-
-    console.log("Approved!, staking now...");
-
-    uint256 validatorId = stakeManager.signerToValidator(stakeFor);
-    console.log("Validator ID : ",validatorId);
-    stakeManager.topUpForFee(stakeFor, amount);
-
-    console.log("Success!");
-
-
-  }
+        console.log("Success!");
+    }
 }
