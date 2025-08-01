d96d5929037d77f10af24404bbd02d3dc794decd
Krishang Shah <109511742+kamuikatsurgi@users.noreply.github.com>
2025-08-01 14:37:03 +0530

    fix: bug fixes and forge fmt (#33)

--- scripts/deployment-scripts/syncChildStateToRoot.s.sol
+++ scripts/deployment-scripts/syncChildStateToRoot.s.sol
@@ -1,6 +1,7 @@
 // SPDX-License-Identifier: MIT
 
 pragma solidity ^0.8.4;
+
 import {Script, stdJson, console} from "forge-std/Script.sol";
 
 import {Registry} from "../helpers/interfaces/Registry.generated.sol";
@@ -8,54 +9,73 @@ import {Governance} from "../helpers/interfaces/Governance.generated.sol";
 import {StateSender} from "../helpers/interfaces/StateSender.generated.sol";
 import {DepositManager} from "../helpers/interfaces/DepositManager.generated.sol";
 
-
-
 contract SyncChildStateToRootScript is Script {
-  function run() public {
+    function run() public {
+        uint256 deployerPrivateKey = vm.envUint("DEPLOYER_PRIVATE_KEY");
 
-    uint256 deployerPrivateKey = vm.envUint("DEPLOYER_PRIVATE_KEY");
+        vm.startBroadcast(deployerPrivateKey);
+        string memory path = "contractAddresses.json";
+        string memory json = vm.readFile(path);
 
-    vm.startBroadcast(deployerPrivateKey);
-    string memory path = "contractAddresses.json";
-    string memory json = vm.readFile(path);
+        address registryAddress = vm.parseJsonAddress(json, ".root.Registry");
 
+        Governance governance = Governance(vm.parseJsonAddress(json, ".root.GovernanceProxy"));
+        Registry registry = Registry(registryAddress);
 
-    address registryAddress = vm.parseJsonAddress(json, '.root.Registry');
+        bytes memory tokenData = abi.encodeWithSelector(
+            bytes4(keccak256("mapToken(address,address,bool)")),
+            vm.parseJsonAddress(json, ".root.tokens.MaticWeth"),
+            vm.parseJsonAddress(json, ".child.tokens.MaticWeth"),
+            false
+        );
+        governance.update(registryAddress, tokenData);
 
-    Governance governance = Governance(vm.parseJsonAddress(json, '.root.GovernanceProxy'));
-    Registry registry = Registry(registryAddress);
+        console.log("Success!");
 
+        tokenData = abi.encodeWithSelector(
+            bytes4(keccak256("mapToken(address,address,bool)")),
+            vm.parseJsonAddress(json, ".root.tokens.MaticToken"),
+            vm.parseJsonAddress(json, ".child.tokens.MaticToken"),
+            false
+        );
+        governance.update(registryAddress, tokenData);
 
-    bytes memory tokenData = abi.encodeWithSelector(bytes4(keccak256("mapToken(address,address,bool)")), vm.parseJsonAddress(json, '.root.tokens.MaticWeth'), vm.parseJsonAddress(json, '.child.tokens.MaticWeth'), false);
-    governance.update(registryAddress, tokenData);
+        console.log("Success!");
 
-    console.log("Success!");
+        tokenData = abi.encodeWithSelector(
+            bytes4(keccak256("mapToken(address,address,bool)")),
+            vm.parseJsonAddress(json, ".root.tokens.TestToken"),
+            vm.parseJsonAddress(json, ".child.tokens.TestToken"),
+            false
+        );
+        governance.update(registryAddress, tokenData);
 
-    tokenData = abi.encodeWithSelector(bytes4(keccak256("mapToken(address,address,bool)")), vm.parseJsonAddress(json, '.root.tokens.MaticToken'), vm.parseJsonAddress(json, '.child.tokens.MaticToken'), false);
-    governance.update(registryAddress, tokenData);
+        console.log("Success!");
 
-    console.log("Success1");
+        tokenData = abi.encodeWithSelector(
+            bytes4(keccak256("mapToken(address,address,bool)")),
+            vm.parseJsonAddress(json, ".root.tokens.RootERC721"),
+            vm.parseJsonAddress(json, ".child.tokens.RootERC721"),
+            true
+        );
+        governance.update(registryAddress, tokenData);
 
-    tokenData = abi.encodeWithSelector(bytes4(keccak256("mapToken(address,address,bool)")), vm.parseJsonAddress(json, '.root.tokens.TestToken'), vm.parseJsonAddress(json, '.child.tokens.TestToken'), false);
-    governance.update(registryAddress, tokenData);
+        bytes memory childChainData = abi.encodeWithSelector(
+            bytes4(keccak256("updateContractMap(bytes32,address)")), keccak256(abi.encodePacked("childChain")), vm.parseJsonAddress(json, ".child.ChildChain")
+        );
+        governance.update(registryAddress, childChainData);
 
-    tokenData = abi.encodeWithSelector(bytes4(keccak256("mapToken(address,address,bool)")), vm.parseJsonAddress(json, '.root.tokens.RootERC721'), vm.parseJsonAddress(json, '.child.tokens.RootERC721'), true);
-    governance.update(registryAddress, tokenData);
+        StateSender stateSenderContract = StateSender(vm.parseJsonAddress(json, ".root.StateSender"));
+        stateSenderContract.register(vm.parseJsonAddress(json, ".root.DepositManagerProxy"), vm.parseJsonAddress(json, ".child.ChildChain"));
 
-    bytes memory childChainData = abi.encodeWithSelector(bytes4(keccak256("updateContractMap(bytes32,address)")), keccak256(abi.encodePacked("childChain")), vm.parseJsonAddress(json, '.child.ChildChain'));
-    governance.update(registryAddress, childChainData);
+        DepositManager depositManager = DepositManager(payable(vm.parseJsonAddress(json, ".root.DepositManagerProxy")));
+        address currentChildChain = depositManager.childChain();
+        address currentStateSender = depositManager.stateSender();
+        (address newChildChain, address newStateSender) = registry.getChildChainAndStateSender();
+        if (currentChildChain != newChildChain || currentStateSender != newStateSender) {
+            depositManager.updateChildChainAndStateSender();
+        }
 
-    StateSender stateSenderContract = StateSender(vm.parseJsonAddress(json, '.root.StateSender'));
-    stateSenderContract.register(vm.parseJsonAddress(json, '.root.DepositManagerProxy'), vm.parseJsonAddress(json, '.child.ChildChain'));
-
-    DepositManager depositManager = DepositManager(payable(vm.parseJsonAddress(json, '.root.DepositManagerProxy')));
-    address currentChildChain = depositManager.childChain();
-    address currentStateSender = depositManager.stateSender();
-    (address newChildChain, address newStateSender) = registry.getChildChainAndStateSender();
-    if (currentChildChain != newChildChain || currentStateSender != newStateSender) {
-      depositManager.updateChildChainAndStateSender();
+        vm.stopBroadcast();
     }
-
-    vm.stopBroadcast();
-  }
 }
