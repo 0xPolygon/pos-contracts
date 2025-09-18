c3386966b85c0bd3548e20c54445fca26409f833
ethyla <36268899+ethyla@users.noreply.github.com>
2025-09-18 12:11:35 +0200

    feat: deployhelper for tests

---
+++ script/setup/ArtifactPath.sol
@@ -0,0 +1,23 @@
+// SPDX-License-Identifier: SEE LICENSE IN LICENSE
+pragma solidity ^0.8.0;
+
+abstract contract ArtifactPath {
+    string StakeManagerPath = "out/StakeManager.sol/StakeManager.json";
+    string StakeManagerExtensionPath = "out/StakeManagerExtension.sol/StakeManagerExtension.json";
+    string StakeManagerProxyPath = "out/StakeManagerProxy.sol/StakeManagerProxy.json";
+    string StakingNFTPath = "out/StakingNFT.sol/StakingNFT.json";
+    string ValidatorSharePath = "out/ValidatorShare.sol/ValidatorShare.json";
+    string ValidatorShareFactoryPath = "out/ValidatorShareFactory.sol/ValidatorShareFactory.json";
+    string EventsHubPath = "out/EventsHub.sol/EventsHub.json";
+    string EventsHubProxyPath = "out/EventsHubProxy.sol/EventsHubProxy.json";
+    string StakingInfoPath = "out/StakingInfo.sol/StakingInfo.json";
+    string RegistryPath = "out/Registry.sol/Registry.json";
+    string GovernancePath = "out/Governance.sol/Governance.json";
+    string GovernanceProxyPath = "out/GovernanceProxy.sol/GovernanceProxy.json";
+    string PolygonMigrationPath = "out/PolygonMigration.sol/PolygonMigration.json";
+    string ERC20PermitPath = "out/ERC20Permit.sol/ERC20Permit.json";
+    string TestTokenPath = "out/TestToken.sol/TestToken.json";
+    string RootChainPath = "out/RootChain.sol/RootChain.json";
+    string RootChainProxyPath = "out/RootChainProxy.sol/RootChainProxy.json";
+    string StateSenderPath = "out/StateSender.sol/StateSender.json";
+}
