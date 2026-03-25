a168cfbb1140a3db990303f93688f88d9e9d07d4
leovct <l009.vincent@gmail.com>
2026-03-25 12:07:51 +0100

    fix: initialize MATIC tokens

--- scripts/deployment-scripts/initializeState.s.sol
+++ scripts/deployment-scripts/initializeState.s.sol
@@ -33,11 +33,10 @@ contract InitializeStateScript is Script {
         updateContractMap(keccak256("withdrawManager"), vm.parseJsonAddress(json, ".root.WithdrawManagerProxy"));
         updateContractMap(keccak256("stakeManager"), vm.parseJsonAddress(json, ".root.StakeManagerProxy"));
         updateContractMap(keccak256("stateSender"), vm.parseJsonAddress(json, ".root.StateSender"));
+        updateContractMap(keccak256("matic"), vm.parseJsonAddress(json, ".root.tokens.MaticToken"));
         updateContractMap(keccak256("pol"), vm.parseJsonAddress(json, ".root.tokens.PolToken"));
         updateContractMap(keccak256("wethToken"), vm.parseJsonAddress(json, ".root.tokens.MaticWeth"));
         updateContractMap(keccak256("eventsHub"), vm.parseJsonAddress(json, ".root.EventsHubProxy"));
-
-        console.log("Success");
 
         bytes memory erc20PredicateData = abi.encodeCall(registry.addErc20Predicate, (vm.parseJsonAddress(json, ".root.predicates.ERC20Predicate")));
         governance.update(registryAddress, erc20PredicateData);
