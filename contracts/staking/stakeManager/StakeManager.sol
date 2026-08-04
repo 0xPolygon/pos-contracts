ea82544adcd6fc19ec051d99cad0a10cb4e32289
ethyla <36268899+ethyla@users.noreply.github.com>
2026-08-04 18:35:43 +0200

    revert: validator permissioning to restore main parity

--- contracts/staking/stakeManager/StakeManager.sol
+++ contracts/staking/stakeManager/StakeManager.sol
@@ -19,8 +19,6 @@ import {IGovernance} from "../../common/governance/IGovernance.sol";
 import {Initializable} from "../../common/mixin/Initializable.sol";
 import {StakeManagerExtension} from "./StakeManagerExtension.sol";
 import {IPolygonMigration} from "../../common/misc/IPolygonMigration.sol";
-import {Registry} from "../../common/Registry.sol";
-import {IValidatorPass} from "./IValidatorPass.sol";
 
 contract StakeManager is
     StakeManagerStorage,
@@ -31,9 +29,6 @@ contract StakeManager is
 {
     using SafeMath for uint256;
     using Merkle for bytes32;
-
-    // Registry key of the optional validator-pass module that permissions new validator entry.
-    bytes32 constant VALIDATOR_PASS_KEY = keccak256("validatorPass");
 
     struct UnsignedValidatorsContext {
         uint256 unsignedValidatorIndex;
@@ -406,17 +401,6 @@ contract StakeManager is
     ) internal {
         require(currentValidatorSetSize() < validatorThreshold, "no more slots");
         require(amount >= minDeposit, "not enough deposit");
-
-        // Permissioned entry: a registered validator-pass module must consume a single-use pass for
-        // the entrant (issued by governance) before funds move; unregistered => permissionless.
-        address validatorPass = Registry(registry).contractMap(VALIDATOR_PASS_KEY);
-        if (validatorPass != address(0)) {
-            require(
-                IValidatorPass(validatorPass).consumePass(user, signerPubkey, amount, msg.sender),
-                "no valid pass"
-            );
-        }
-
         _transferAndTopUp(user, msg.sender, heimdallFee, amount, pol);
         _stakeFor(user, amount, acceptDelegation, signerPubkey);
     }
